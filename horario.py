dbc346bbb0359b9b591bbf46c1f954fe5ce782f8
gabrielfelipedy <gabriel.felipe.dy@gmail.com>
2023-10-19 13:21:11 -0300

    add gitignore and regex validation

--- horario.py
+++ horario.py
@@ -1,2 +1,24 @@
-class Horario:
+import re
 
+class Horario:
+    def __valid_code(self, code):
+        code = code.split(" ")
+        codes = []
+
+        for horario in code:
+            horario_regex = re.compile(r'^([1-7]){1,7}([ntNT])([1-5]){1,5}')
+            result = horario_regex.search(horario)
+
+            if result != None:
+                codes.append(result.group(0))
+
+        if(len(codes) != 0):
+            return codes
+        else:
+            return []
+
+    def __init__(self, code) -> None:
+        self.__code = self.__valid_code(code)
+
+    def get_code(self):
+        return self.__code
--- main.py
+++ main.py
@@ -2,3 +2,8 @@
 # Receber entradas do usuário para os horários
 # Organizar em uma tabela
 # Mostrar com interface gráfica
+from horario import Horario
+
+if __name__ == "__main__":
+    horario = Horario("6T1345 3T5")
+    print(horario.get_code())
